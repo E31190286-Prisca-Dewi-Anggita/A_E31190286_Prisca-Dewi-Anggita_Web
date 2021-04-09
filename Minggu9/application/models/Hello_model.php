32c32ee8cdb05efba1a7ffe712d77cbc3e16c72d
E31190286-Prisca-Dewi-Anggita <priscadewi08@gmail>
2021-04-09 15:10:23 +0700

    E31190286_PriscaDewiAnggita

---
+++ Minggu9/application/models/Hello_model.php
@@ -0,0 +1,12 @@
+<?php
+class Hello_model extends CI_Model{
+    //public $txt = 'Hello World!';
+    
+    public $alert = 'Hello Wolrd dari CI Model';
+    
+    public $mahasiswa = [
+			["nama"=>"Adya Zalfa","nim"=>"E31190957","gol"=>"B"],
+			["nama"=>"Elysha Fitriana  ","nim"=>"E31190416","gol"=>"A"],
+			["nama"=>"Risma Raudlatul","nim"=>"E31190596","gol"=>"A"] 
+		];
+}
