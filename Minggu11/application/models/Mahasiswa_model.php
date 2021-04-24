ca7cec4a761dbec2f6d43c1dd36ac4db080dcc1e
E31190286-Prisca-Dewi-Anggita <priscadewi08@gmail>
2021-04-24 20:21:34 +0700

    Minggu11

---
+++ Minggu11/application/models/Mahasiswa_model.php
@@ -0,0 +1,29 @@
+<?php 
+// membuat class model, dengan syarat nama file sama dengan nama Class
+class Mahasiswa_model extends CI_Model {
+	// idenfikasi variable
+	public function get_data(){ //memanggil fungsi get data
+		$data_mahasiswa = [ //data dari mahasiswa
+			["nama"=>"Prisca Dewi Anggita","prodi"=>"MIF"],
+			["nama"=>"Rania","prodi"=>"TKK"],
+			["nama"=>"Tania","prodi"=>"TIF"]
+		];
+		// mengembalikan nilai variabel data angkatan
+		return $data_mahasiswa;
+	}
+
+	public function getAll(){ //method untuk mengambil data dari database
+		$this->db->select('*');
+		$this->db->from('tm_user');
+		$this->db->join('tm_grup','tm_user.grup = tm_grup.id_grup');
+		$query = $this->db->get();
+		//mengembalikan nilai variabel query
+		return $query;
+	}
+
+	public function input_data($data,$table) //memanggil fungsi input data
+	{
+		$this->db->insert($table,$data); //berfungsi untuk menyimpan data ke database 
+	}
+}
+?>
